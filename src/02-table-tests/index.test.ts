2b9afeb91f1137f3736a8892389d21410fd00db5
aleks_pv <apanov08@gmail.com>
2023-07-02 22:52:24 +0400

    feat: add simple-tests table-tests error-handling-async

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,43 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+ import {  simpleCalculator, Action } from './index';
 
 const testCases = [
     { a: 1, b: 2, action: Action.Add, expected: 3 },
     { a: 2, b: 2, action: Action.Add, expected: 4 },
     { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { a: 4, b: 1, action: Action.Subtract, expected: 3 },
+    { a: 40, b: 21, action: Action.Subtract, expected: 19 },
+    { a: 30, b: 2, action: Action.Subtract, expected: 28 },
+    { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+    { a: 38, b: 2, action: Action.Multiply, expected: 76 },
+    { a: 13, b: 20, action: Action.Multiply, expected: 260 },
+    { a: 100, b: 2, action: Action.Divide, expected: 50 },
+    { a: 300, b: 2, action: Action.Divide, expected: 150 },
+    { a: 46, b: 2, action: Action.Divide, expected: 23 },
+    { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+    { a: 4, b: 2, action: Action.Exponentiate, expected: 16 },
+    { a: 6, b: 2, action: Action.Exponentiate, expected: 36 },
+]; 
 
 describe('simpleCalculator', () => {
   // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach((testCase) => {
+    const { a, b, action, expected } = testCase;
+
+    test(`should return ${expected} for ${a} ${action} ${b}`, () => {
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
+    });
+  });
+
+  test('should return null for invalid action', () => {
+    const result = simpleCalculator({ a: 5, b: 3, action: '[' });
+    expect(result).toBeNull();
+  });
+
+  test('should return null for invalid action', () => {
+    const result = simpleCalculator({ a: 5, b: 3, action: 'invalidAction' });
+    expect(result).toBeNull();
   });
   // Consider to use Jest table tests API to test all cases above
 });
